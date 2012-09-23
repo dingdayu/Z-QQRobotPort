ef8b709846ba17dd05a66db54ef45e0f000ea6b2
dingdayu <614422099@qq.com>
2012-09-23 23:10:46 +0800

    Signed-off-by: 614422099@qq.com <614422099@qq.com>
    增加开发目录极其文件所产生的改动

---
+++ Development/Development.php
@@ -0,0 +1,16 @@
+<?php
+
+    $Sender = trim(isset($_POST["Sender"])?$_POST["Sender"]:'');//发送消息的QQ号码
+    $Message = trim(isset($_POST["Message"])?$_POST["Message"]:'');//发送的消息
+    $Nick = trim(isset($_POST["Nick"])?$_POST["Nick"]:'');//QQ昵称
+    $Event = trim(isset($_POST["Event"])?$_POST["Event"]:'');//事件类型
+    $Qunid = trim(isset($_POST["ClusterNum"])?$_POST["ClusterNum"]:'');//群号码
+    $QunName = trim(isset($_POST["ClusterName"])?$_POST["ClusterName"]:'');//群名称
+    $ApiPort = trim(isset($_POST["ApiPort"])?$_POST["ApiPort"]:'');
+    $RobotQQ = trim(isset($_POST["RobotQQ"])?$_POST["RobotQQ"]:'');
+    $SendTime = trim(isset($_POST["SendTime"])?$_POST["SendTime"]:'');//收到消息事件的时间戳
+    $Version = trim(isset($_POST["Version"])?$_POST["Version"]:'');//机器人客户端版本号
+    $CCopyright = trim(isset($_POST["Copyright"])?$_POST["Copyright"]:'');//接口密匙
+	
+	$txt = fopen("Development_Test.txt","a") ;
+	fwrite($txt,"\r\nSender:\t\t".$Sender."\r\nMessage:\t".$Message."\r\nNick:\t\t".$Nick."\r\nEvent:\t\t".$Event."\r\nClusterNum:\t".$Qunid."\r\nClusterName:\t".$QunName."\r\nApiPort:\t".$ApiPort."\r\nRobotQQ:\t".$RobotQQ."\r\nSendTime:\t".$SendTime."\r\nVersion:\t".$Version."\r\nCopyright:\t".$CCopyright."\r\n----------------------------\r\n");
--- README.txt
+++ README.txt
@@ -1,6 +1,7 @@
 ﻿
 	Z-QQRobot Port 项目
 	
+	欢迎各位朋友贡献代码！
 	
 	文档结构说明：
 	
@@ -8,7 +9,6 @@
 		┃	┣	━━━━
 		┃	┣ README.txt					接口读我
 		┃	┣ Api List.txt					Api 列表
-		┃	┣ Development description.txt			开发说明
 		┃	┣ LICENSE.txt					版权文件
 		┃	┣	━━━━
 		┃	┣ index.php					测试文档
@@ -20,5 +20,12 @@
 		┃	┣ Huifu.php					快速回复文件
 		┃	┣ Api.php					功能文件
 		┃	┗ Even.php					特殊事件回复
+		┃
+		┣ Development ━
+		┃	┣	━━━━
+		┃	┣ Development description.txt			开发说明
+		┃	┣ Development_Test.txt				post接受接口产生的记录文件
+		┃	┣	━━━━
+		┃	┗ Development.php				POST接受文件
 		┃
 		┗ README.txt					项目读我
