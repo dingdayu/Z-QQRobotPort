e2dd872a2c9b6cee5d6f57303e76abcd7a05e4f2
dingdayu <614422099@qq.com>
2012-09-25 12:49:19 +0800

    Signed-off-by: dingdayu <614422099@qq.com>
    修复API命令的管理员识别部分
    注：API命令需在配置文件内正确填写机器人ip和密钥

--- QQRobotPort/QQRobotApi.php
+++ QQRobotPort/QQRobotApi.php
@@ -9,42 +9,52 @@
 // +----------------------------------------------------------------------
 // //------------------- // 机器人Api操作区 //----------------------- // //
 
-   if(in_array($Message,$Api_ResetRobot)){  	//重启机器人命令
-         echo RobotApiSend('ResetRobot');
-         exit;
-    }
+	if(in_array($Sender,$Adminqq)){ 		//需要验证的命令
+		if(in_array($Message,$Api_ResetRobot)){  	//重启机器人命令
+			echo RobotApiSend('ResetRobot');
+			exit;
+		}
 	
-	if(in_array($Message,$Api_LoginRobot)){  	//登陆机器人命令
-         echo RobotApiSend('LoginRobot');
-         exit;
-    }
+		if(in_array($Message,$Api_LoginRobot)){  	//登陆机器人命令
+			echo RobotApiSend('LoginRobot');
+			exit;
+		}
 	
-	if(in_array($Message,$Api_ExitRobot)){  	//退出机器人命令
-         echo RobotApiSend('ExitRobot');
-         exit;
-    }
+		if(in_array($Message,$Api_ExitRobot)){  	//退出机器人命令
+			echo RobotApiSend('ExitRobot');
+			exit;
+		}
 	
+		if(in_array($Message,$Api_Friend_K)){  		//开启好友回复
+			echo RobotApiSend('Friend','','1');
+			exit;
+		}
+	
+		if(in_array($Message,$Api_Friend_G)){  		//关闭好友回复
+			echo RobotApiSend('Friend','','0');
+			exit;
+		}
+	
+		if(in_array($Message,$Api_Cluster_K)){  		//开启群回复
+			echo RobotApiSend('Cluster','','1');
+			exit;
+		}
+	
+		if(in_array($Message,$Api_Cluster_G)){  		//关闭群回复
+			echo RobotApiSend('Cluster','','0');
+			exit;
+		}
+	
+	}
+	
+	/*以下不限制管理员QQ的命令！*/
+	
+		
 	if(in_array($Message,$Api_LoginStatus)){ 	//返回当前登录状态
-         echo RobotApiSend('LoginStatu');
-         exit;
-    }
+		echo RobotApiSend('LoginStatu');
+		exit;
+	}
 	
-	if(in_array($Message,$Api_Friend_K)){  		//开启好友回复
-         echo RobotApiSend('Friend','','1');
-         exit;
-    }
-	
-	if(in_array($Message,$Api_Friend_G)){  		//关闭好友回复
-         echo RobotApiSend('Friend','','0');
-         exit;
-    }
-	
-	if(in_array($Message,$Api_Cluster_K)){  		//开启群回复
-         echo RobotApiSend('Cluster','','1');
-         exit;
-    }
-	
-	if(in_array($Message,$Api_Cluster_G)){  		//关闭群回复
-         echo RobotApiSend('Cluster','','0');
-         exit;
+	if($Event == 'ReceiveKickOut'){					
+		$str = file_get_contents("http://".$QQRobotIp.":".$ApiPort."/Api?Key=".$Copyright."&SendType=ReStartRobot");
     }
